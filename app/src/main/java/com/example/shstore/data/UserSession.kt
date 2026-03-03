5cd560676d9db3b4a20afd0ee27d2e9f7332d168
lilsosix <gord.balakhonow@gmail.com>
2026-03-03 14:26:31 +0300

    Доделан экран смены пароля, корректно импортированы графические ресурсы

--- app/src/main/java/com/example/shstore/data/UserSession.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shstore.data
-
-object UserSession {
-    // id пользователя из auth.users (uuid)
-    var userId: String? = null
-
-    // access_token, который вернул Supabase при signIn
-    var accessToken: String? = null
-}
